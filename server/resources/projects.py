f39d1a5171a576a5e105040fb0a9e5f5cfff3741
KoupendraDB <koupenda.dandapani@ferguson.com>
2024-04-10 20:47:40 +0530

    project user ids fix

--- server/resources/projects.py
+++ server/resources/projects.py
@@ -14,22 +14,22 @@ class Projects(Resource):
         self.project_database = mongo_client[database].project
         self.project_cache_controller = RedisCacheController(redis_client, self.project_masker, self.cache_time)
 
-    def fetch_user_projects(self, user):
-        cached_data = self.project_cache_controller.get_cache('user:{}:projects', str(user['_id']))
+    def fetch_user_projects(self, user_id):
+        cached_data = self.project_cache_controller.get_cache('user:{}:projects', str(user_id))
         if cached_data:
             return cached_data
         projects = self.project_database.find({'$or': [
-                {'project_manager': user['_id']},
-                {'developers': {'$elemMatch': {'$eq': user['_id']}}},
-                {'qas': {'$elemMatch': {'$eq': user['_id']}}}
+                {'project_manager': user_id},
+                {'developers': {'$elemMatch': {'$eq': user_id}}},
+                {'qas': {'$elemMatch': {'$eq': user_id}}}
         ]})
         projects = [project for project in projects]
-        self.project_cache_controller.set_cache('user:{}:projects', user['_id'], projects)
+        self.project_cache_controller.set_cache('user:{}:projects', user_id, projects)
         return projects
 
     @token_required
-    def get(self, user):
-        project = self.fetch_user_projects(user)
+    def get(self, user_id, user):
+        project = self.fetch_user_projects(user_id)
         return {
             'success': True,
             'projects': [unmask_fields(project, self.project_masker) for project in project]
@@ -40,7 +40,9 @@ class Projects(Resource):
         project_request = request.get_json()
         insert_result = self.project_database.insert_one(project_request)
         if insert_result and insert_result.inserted_id:
-            self.project_cache_controller.delete_cache('user:{}:projects', user['_id'])
+            users = project_request['developers'] + project_request['qas'] + [project_request['project_manager'], project_request['admin']]
+            for user_id in users:
+                self.project_cache_controller.delete_cache('user:{}:projects', user_id)
             result = {'success': True, 'project_id': str(insert_result.inserted_id)}, 201
             return result
         return {'success': False}, 400
