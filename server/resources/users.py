246686585c8de9282cfd93d45c3878f462614032
KoupendraDB <kbhuvaneswari@salesforce.com>
2024-04-18 13:52:53 +0530

    removed masking from users query

--- server/resources/users.py
+++ server/resources/users.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, request
 from bson.objectid import ObjectId
-from .helpers.masker import unmask_fields, mask_fields
+from .helpers.masker import unmask_fields
 
 class Users(Resource):
     def __init__(self):
@@ -14,8 +14,7 @@ class Users(Resource):
         self.user_database = mongo_client[database].user
 
     def fetch_users(self, request_query):
-        query = mask_fields(request_query, self.user_masker)
-        users = self.user_database.find(query, {'_id': 0, 'password': 0})
+        users = self.user_database.find(request_query, {'_id': 0, 'password': 0})
         return users if users else []
 
     def get(self):
